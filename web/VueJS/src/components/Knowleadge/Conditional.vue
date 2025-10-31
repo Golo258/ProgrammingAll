1bed1e018006401aed26aee3fac75cbc26043765
Golo258 <golon338@gmail.com>
2025-10-31 14:14:06 +0100

    [Web] Add web, javascript, vue, nodejs and others

---
+++ web/VueJS/src/components/Knowleadge/Conditional.vue
@@ -0,0 +1,26 @@
+<template>
+    <h2 v-if="numb === 0">The number is zero</h2>
+    <h2 v-else-if="numb === 2">The number is two</h2>
+    <h2 v-else>The number is different</h2>
+    <div v-show="isDivDisplayed" >
+        <ul>
+            <li>One</li>
+            <li>Two</li>
+            <li>Three</li>
+        </ul>
+    </div>
+
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      numb: 5,
+      isDivDisplayed: false,
+    };
+  },
+};
+</script>
+
