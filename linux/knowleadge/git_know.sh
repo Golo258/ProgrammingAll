25ea890a7b9b84480a2307f99d989eaf6e50282b
Grzegorz Golonka <golon338@gmail.com>
2025-12-13 22:46:13 +0000

    [git] add script to change commiter credentials

--- linux/knowleadge/git_know.sh
+++ linux/knowleadge/git_know.sh
@@ -345,3 +345,22 @@ Problem z credentialami:
 
     Kopiowanie:
         ssh-copy-id -i ~/.ssh/id_ed25519.pub user@host
+
+
+# Skrypt do ustawiania danej osoby jako coommitera
+git filter-branch --env-filter '
+OLD_EMAIL="grzegorz.1.golonka@nokia.com"
+CORRECT_NAME="Grzegorz Golonka"
+CORRECT_EMAIL="golon338@gmail.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
